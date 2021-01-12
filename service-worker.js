315c8d6a1a12833e314845473e0bcc739365fd66
ShyianO <ol.shyian@gmacil.com>
2021-01-12 15:45:54 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.2bcbb7d5489d4ec389b867bc038ca23d.js"
 );
 
 self.addEventListener('message', (event) => {
